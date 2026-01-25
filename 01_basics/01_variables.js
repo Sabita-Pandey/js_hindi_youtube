92e84f7743cd558bf03b7a67c480311fd63cdb8a
Sabita pandey <sp1529460@gmail.com>
2026-01-25 18:40:17 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "sabita@gmail.com"
+var accountPassword = "123456"
+accountCity = "Jaipur"
+let accountState;
+
+//accountId = 2 // not allowed
+ // {}=this braces is called scope
+accountEmail = "sp1234@gmail.com"
+accountPassword = "8764569"
+accountCity = "Koderma"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as newar version
+
+//alert("Hello") //we are using nodejs, not browser
+
+console.log(3+3) //code readability should be high
+
+console.log("Sabita Pandey")
+
+
+
+let name = "Sabita" // String data type
+let age = 21 // number datatype
+let isLoggedIn = false //boolean type data type
+
+
+// number => 2 to power 53
+//bigint
+//string => "" , ''
+//boolean => true/false
+//null => standalone value
+// undefined =>
+// symbol => use to find uniqueness or unique
+console.log(typeof undefined);
+
+
+// Null => It is object in javascript
+// Undefined => It is also called undefined because it is a type.
