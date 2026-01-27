51bc82f8ae3f096bf1354c735f9800271e6d6036
Sabita pandey <sp1529460@gmail.com>
2026-01-27 12:17:46 +0000

    conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -4,10 +4,38 @@
 // let score = "null" => // string, string, number NaN
 // let score = null => // object, object, number 0
 //let score = undefined => //undefined, undefined, number Nan
-let score = true
-console.log(typeof score);
-console.log(typeof (score)); 
+// let score = true => // boolean, boolean, number, 1
+//let score = "Sabita Pandey" => // string, string, number, NaN
+ let score = "Sabita"
 
-let valueinNumber = Number(score)
-console.log(typeof valueinNumber);
-console.log(valueinNumber)
+
+/*console.log(typeof score);
+  console.log(typeof (score)); */
+
+  let valueinNumber = Number(score)
+  /*console.log(typeof valueinNumber);
+  console.log(valueinNumber)*/
+
+
+//"33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+// let IsloggedIn = 1 => true
+// let IsloggedIn = "" => false
+// let IsloggedIn = "Sabita" => true
+let IsloggedIn = 1
+ 
+let booleanisLoggedIn = Boolean(IsloggedIn)
+//console.log(booleanisLoggedIn)
+
+//1 => true; 0 => false
+// "" => false
+// "sabita" => true
+
+let someNumber = 45
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
